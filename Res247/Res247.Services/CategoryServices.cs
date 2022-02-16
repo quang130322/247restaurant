69f304f68786f298061f8efbee717e5c07839bae
krisdo0 <bacdvhe141303@fpt.edu.vn>
2022-02-16 09:59:00 +0700

    update GetCategoriesIdByFood

--- Res247/Res247.Services/CategoryServices.cs
+++ Res247/Res247.Services/CategoryServices.cs
@@ -14,5 +14,25 @@ namespace Res247.Services
         public CategoryServices(IUnitOfWork unitOfWork) : base(unitOfWork)
         {
         }
+
+        public List<int> GetCategoriesIdByFood(int id, bool isIncludeDelete = false)
+        {
+            List<int> categoriesId = new List<int>();
+            if (!isIncludeDelete)
+            {
+                var categories = _unitOfWork.CategoryRepository.GetQuery(x => x.Foods.Any(f => f.Id == id && f.IsDeleted == isIncludeDelete)).ToList();
+                foreach (var item in categories)
+                {
+                    categoriesId.Add(item.Id);
+                }
+                return categoriesId;
+            }
+            var list = _unitOfWork.CategoryRepository.GetQuery(x => x.Foods.Any(f => f.Id == id)).ToList();
+            foreach (var item in list)
+            {
+                categoriesId.Add(item.Id);
+            }
+            return categoriesId;
+        }
     }
 }
